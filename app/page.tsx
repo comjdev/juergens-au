fc33ef6b4ee7ac356a8e3ab098755450156f057c
Christoph <christoph@juergens.au>
2026-01-07 15:42:34 +1100

    Update job title on homepage to 'Senior Software Engineer'

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
 		<main className="bg-black-100">
 			<div className="text-white flex flex-col items-center justify-center h-screen">
 				<h1 className="text-4xl font-bold">Christoph Jürgens</h1>
-				<p className="text-lg">Software Engineer</p>
+				<p className="text-lg">Senior Software Engineer</p>
 				<p className="text-sm">Melbourne, Australia</p>
 			</div>
 		</main>
