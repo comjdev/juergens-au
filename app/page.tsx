8fe7c81152b4709bf69dc82fdc81c38eb67b46c3
Christoph <christoph@juergens.au>
2026-01-10 08:21:39 +1100

    projects and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaHome, FaUser, FaEnvelope } from "react-icons/fa";
 import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
 
 export default function Home() {
 	return (
 		<main className="relative bg-black-100 flex items-center justify-center flex-col overflow-hidden mx-auto sm:px10 px-5">
 			<div className="max-w-7xl w-full">
-				<FloatingNav
-					navItems={[
-						{ name: "Home", link: "/", icon: <FaHome /> },
-						{ name: "About", link: "/about", icon: <FaUser /> },
-						{ name: "Contact", link: "/contact", icon: <FaEnvelope /> },
-					]}
-				/>
+				<FloatingNav navItems={navItems} />
 				<Hero />
 				<Grid />
+				<RecentProjects />
 			</div>
 		</main>
 	);
